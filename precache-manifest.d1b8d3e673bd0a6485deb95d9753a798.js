ddeca154ea61f9b4d0d1de09f835ba536888dbaa
patw <patw.hi@gmail.com>
2019-01-24 20:11:44 +0800

    Updates

---
+++ precache-manifest.d1b8d3e673bd0a6485deb95d9753a798.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a79c0a1ebae991f889ea",
+    "url": "/udemy-react-hooks-practice/static/js/runtime~main.a79c0a1e.js"
+  },
+  {
+    "revision": "272341c2289fdee76152",
+    "url": "/udemy-react-hooks-practice/static/js/main.272341c2.chunk.js"
+  },
+  {
+    "revision": "028df38ef094c6411aa0",
+    "url": "/udemy-react-hooks-practice/static/js/1.028df38e.chunk.js"
+  },
+  {
+    "revision": "d3d57b36a8910c1a1eb8de613b45bd8d",
+    "url": "/udemy-react-hooks-practice/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4cf5c42ea92b58029d7557caa9956df.js"
+  "/udemy-react-hooks-practice/precache-manifest.d1b8d3e673bd0a6485deb95d9753a798.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/udemy-react-hooks-practice/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
