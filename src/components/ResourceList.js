c35e997a89b3942e73e482ee4c20f3ffa4f35a72
patw <patw.hi@gmail.com>
2019-01-24 18:25:36 +0800

    Use useState & useEffect to refactor resource list to functional-based component

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default class ResourceList extends Component {
-  state = {
-    resources: [],
+export default ({ resource }) => {
+  const [resources, setResources] = useState([]);
+
+  const fetchResources = async () => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+
+    setResources(response.data);
   };
 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+  useEffect(() => {
+    fetchResources();
+  }, [resource]);
 
-    this.setState({
-      resources: response.data,
-    });
-  }
-
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-
-      this.setState({
-        resources: response.data,
-      });
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.resources.length}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {resources.length}
+    </div>
+  );
 }
